22033599e33351cb4fd53efd068ad9d6cdc84e01
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-27 14:16:39 +0200

    catalogitem objects will be mapped in the form

--- src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
+++ src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
@@ -1,6 +1,8 @@
 package com.linkedin.jsf;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
@@ -9,6 +11,20 @@ import javax.inject.Named;
 @Named
 public class CatalogItemFormBean implements Serializable {
 	
+	private CatalogItem item = new CatalogItem();
+	private List<CatalogItem> items = new ArrayList<>();
 	
-
+	public CatalogItem getItem() {
+		return item;
+	}
+	public void setItem(CatalogItem item) {
+		this.item = item;
+	}
+	public List<CatalogItem> getItems() {
+		return items;
+	}
+	public void setItems(List<CatalogItem> items) {
+		this.items = items;
+	}
+	
 }

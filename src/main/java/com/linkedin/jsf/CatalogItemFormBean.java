42d7408715fe3ae48bdcbd06fbb001a9798ff26a
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-27 14:09:26 +0200

    prepare refactoring

--- src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
+++ src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
@@ -1,34 +1,13 @@
 package com.linkedin.jsf;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
-@RequestScoped
+@SessionScoped
 @Named
-public class CatalogItemFormBean {
-	
-	private String name;
-	private String manufacturer;
-	private String sku;
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getManufacturer() {
-		return manufacturer;
-	}
-	public void setManufacturer(String manufacturer) {
-		this.manufacturer = manufacturer;
-	}
-	public String getSku() {
-		return sku;
-	}
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
+public class CatalogItemFormBean implements Serializable {
 	
 	
 

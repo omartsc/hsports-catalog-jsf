758a7fd36ad5defa3201c9e34a21ed5f916b235f
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-27 14:39:20 +0200

    add method to create and add new items

--- src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
+++ src/main/java/com/linkedin/jsf/CatalogItemFormBean.java
@@ -10,21 +10,36 @@ import javax.inject.Named;
 @SessionScoped
 @Named
 public class CatalogItemFormBean implements Serializable {
-	
+
 	private CatalogItem item = new CatalogItem();
 	private List<CatalogItem> items = new ArrayList<>();
-	
+
+	public String addItem() {
+		long itemId = this.items.size() + 1;
+
+		this.items.add(new CatalogItem(itemId, this.item.getName(), this.item.getManufacturer(),
+				this.item.getDescription(), this.item.getAvailableDate()));
+
+		this.items.stream().forEach(item -> {
+			System.out.println(item.toString());
+		});
+		return "list?faces-redirect=true";
+	}
+
 	public CatalogItem getItem() {
 		return item;
 	}
+
 	public void setItem(CatalogItem item) {
 		this.item = item;
 	}
+
 	public List<CatalogItem> getItems() {
 		return items;
 	}
+
 	public void setItems(List<CatalogItem> items) {
 		this.items = items;
 	}
-	
+
 }
